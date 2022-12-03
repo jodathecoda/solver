a1bb3c3c2a648474c2c334df973966370334266b
JodaTheCoda <jodathecoda@yahoo.com>
2022-12-03 20:30:31 +0200

    kkk

--- test.py
+++ test.py
@@ -1,30 +1,17 @@
-import tkinter as tk
-import sys
+import matplotlib.pyplot as plt
 
-class PrintLogger(): # create file like object
-    def __init__(self, textbox): # pass reference to text widget
-        self.textbox = textbox # keep ref
+fig = plt.figure()
+ax = fig.add_subplot(111)
+text = ax.text(1, 1, 'Text')
 
-    def write(self, text):
-        self.textbox.insert(tk.END, text) # write text to textbox
-            # could also scroll to end of textbox here to make sure always visible
+def on_mouse_move(event):
+    if None not in (event.xdata, event.ydata):
+        text.set_position((event.xdata, event.ydata))
+        if event.xdata > 0.5:
+            text.set_text("ooooo")
+        else:
+            text.set_text("uuu")
+        fig.canvas.draw()
 
-    def flush(self): # needed for file like object
-        pass
-
-if __name__ == '__main__':
-    def do_something():
-        print('i did something')
-        root.after(1000, do_something)
-
-    root = tk.Tk()
-    t = tk.Text()
-    t.pack()
-    # create instance of file like object
-    pl = PrintLogger(t)
-
-    # replace sys.stdout with our object
-    sys.stdout = pl
-
-    root.after(1000, do_something)
-    root.mainloop()
+fig.canvas.mpl_connect('motion_notify_event', on_mouse_move)
+plt.show()
