609fc7726403cfdfff7e3b72758fe0edef0550f5
Joda TheCoda <jodathecoda@yahoo.com>
2022-12-19 10:39:51 +0200

    AA for testing

--- aa_config.py
+++ aa_config.py
@@ -2,47 +2,51 @@ buttonAA.config(bg = '#BDBDB7')
     if 'AA' in selected_range:
         if var.get()  == 1: #A
             prio = 0
+            counter_lightblue = 0
+            counter_blue = 0
+            counter_red = 0
+            counter_green = 0
+            counter_orange = 0
+            counter_yellow = 0
             for hand in expander.expandPP('AA'):
                 for hh in a_check_fold:
                     if hand == hh.pohand:
+                        counter_blue += 1
                         if prio < 1:
                             prio = 1
                 for hh in a_bet_fold:
                     if hand == hh.pohand:
-                        ###print("2")
+                        counter_lightblue += 1
                         if prio < 2:
                             prio = 2
-                            ##print("hand: " + hand + " prio: " + str(prio))
                 for hh in a_check_call:
                     if hand == hh.pohand:
-                        ###print("3")
+                        counter_green += 1
                         if prio < 3:
                             prio = 3
-                            ##print("hand: " + hand + " prio: " + str(prio))
                 for hh in a_bet_call:
                     if hand == hh.pohand:
-                        ###print("4")
+                        counter_orange += 1
                         if prio < 4:
                             prio = 4
-                            ##print("hand: " + hand + " prio: " + str(prio))
                 for hh in a_check_raise:
                     if hand == hh.pohand:
-                        ##print(hand + " 5")
+                        counter_red += 1
                         if prio < 5:
                             prio = 5
-                            ##print("hand: " + hand + " prio: " + str(prio))
-            if prio == 5:
-                buttonAA.config(bg = 'red')
-            elif prio == 4:
-                buttonAA.config(bg = 'orange')
-            elif prio == 3:
-                buttonAA.config(bg = 'green')
-            elif prio == 2:
-                buttonAA.config(bg = 'lightblue')
-            elif prio == 1:
-                buttonAA.config(bg = 'blue')
+            colorlist = []
+            colorlist.append(counter_blue)
+            colorlist.append(counter_lightblue)
+            colorlist.append(counter_green)
+            colorlist.append(counter_orange)
+            colorlist.append(counter_red)
+            colorlist.sort(reverse=True)
+            if colorlist[0] > colorlist[1]:
+                #color the button with the most common action occurence,
+                #if two are equal, color it purple
+                buttonAA.config(bg = colorlist[0])
             else:
-                pass
+                buttonAA.config(bg ='purple')
         elif var.get() == 2: #B
             prio = 0
             for hand in expander.expandPP('AA'):
